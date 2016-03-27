dbb3b3a29f46ffaab07c55825f7d70cb350a76a3
Mattia Basaglia <mattia.basaglia@gmail.com>
2016-03-27 13:02:55 +0100

    Re-license Melanolib as GPLv3+

--- c++-compat.hpp
+++ c++-compat.hpp
@@ -1,21 +1,22 @@
 /**
  * \file
- * \brief This file defined some compatibility for features of C++14 and beyond
+ *
  * \author Mattia Basaglia
- * \copyright Copyright 2015-2016 Mattia Basaglia
- * \section License
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU Affero General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Affero General Public License for more details.
+ * \copyright Copyright (C) 2015-2016 Mattia Basaglia
  *
- *  You should have received a copy of the GNU Affero General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef MELANOLIB_CXX_COMPAT_HPP
 #define MELANOLIB_CXX_COMPAT_HPP
--- library.cpp
+++ library.cpp
@@ -3,7 +3,7 @@
  *
  * \author Mattia Basaglia
  *
- * \copyright Copyright 2015-2016 Mattia Basaglia
+ * \copyright Copyright (C) 2015-2016 Mattia Basaglia
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,7 +17,6 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 #include "library.hpp"
