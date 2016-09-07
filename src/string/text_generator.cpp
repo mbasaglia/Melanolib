f40d41ce21895994bb617df39f26d817150860fc
Mattia Basaglia <mattia.basaglia@gmail.com>
2016-09-07 19:38:20 +0100

    Add some comments

--- src/string/text_generator.cpp
+++ src/string/text_generator.cpp
@@ -33,10 +33,12 @@ void TextGenerator::add_text(std::istream& stream)
     Chain::iterator last = chain.end();
     while ( stream.good() )
     {
+        // skip whitespaces
         int ch = ' ';
         while ( ascii::is_space(ch) && stream.good() )
         {
             ch = stream.get();
+            // On newline, mark the last item as a good ending point
             if ( ch == '\n' )
             {
                 prefix = Prefix(prefix_size);
@@ -48,12 +50,15 @@ void TextGenerator::add_text(std::istream& stream)
             break;
         stream.unget();
 
+        // Read a word from the stream and add it to the chain
         if ( stream >> word )
         {
+            // If the current word ends with a period, is a good ending point
             bool end = word.back() == '.' || stream.eof();
             last = chain.insert({prefix, {word, end, Clock::now()}});
             prefix.shift(word);
 
+            // Ensure we don't exceed the maximum size
             if ( chain.size() > max_size )
                 cleanup();
         }
@@ -92,8 +97,11 @@ void TextGenerator::cleanup()
     auto now = Clock::now();
     auto death = Clock::now() - max_age;
 
+    // If the last cleanup was recent, there's no point in trying to erase
+    // old transitions as they would already been removed
     if ( last_cleanup < death )
     {
+        // Remove all entries that are too old
         for ( Chain::iterator iter = chain.begin(); iter != chain.end(); )
         {
             if ( iter->second.creation < death )
@@ -103,6 +111,7 @@ void TextGenerator::cleanup()
         }
     }
 
+    // Remove random transitions until we get to the maximum allowed size
     while ( chain.size() > max_size )
     {
         Chain::iterator iter = chain.begin();
@@ -120,10 +129,13 @@ void TextGenerator::generate_words(Prefix prefix,
 {
     while ( words.size() < max_words )
     {
+        // Select a random suffix for the current prefix
         auto iter = random_suffix(prefix);
+        // If we can't continue, terminate here
         if ( iter == chain.end() )
-            return;
+            break;
         words.push_back(iter->second.text);
+        // If we have enough words and we can end, we terminate
         if ( words.size() >= min_words && iter->second.end )
             break;
         prefix.shift(words.back());
@@ -135,12 +147,19 @@ TextGenerator::Prefix TextGenerator::walk_back(
     std::size_t max_words,
     std::vector<std::string>& words) const
 {
+    // Nothing to do
     if ( words.empty() )
         return Prefix(prefix_size);
+
+    // Keep going back until we hit the word limit
     while ( words.size() < max_words )
     {
         std::vector<Chain::const_iterator> prefixes;
+
+        // single_word is for when we are only supposed to check one word in lookback
         bool single_word = words.size() == 1 || prefix_size == 1;
+
+        // Search through the map for matching transitions
         for ( auto iter = chain.begin(); iter != chain.end(); ++iter )
         {
             if ( single_word )
@@ -154,11 +173,14 @@ TextGenerator::Prefix TextGenerator::walk_back(
                 prefixes.push_back(iter);
             }
         }
+        // No match, bail out
         if ( prefixes.empty() )
             break;
 
+        // Select a random match
         auto random_iter = prefixes[math::random(prefixes.size() - 1)];
 
+        // If it looks like an ending point, exit
         if ( random_iter->second.end && words.size() > 1 )
         {
             words.erase(words.begin());
@@ -171,13 +193,17 @@ TextGenerator::Prefix TextGenerator::walk_back(
         auto prefix_end = random_prefix.end();
         if ( !single_word && random_prefix.begin() != prefix_end  )
             prefix_end--;
+        // Skip empties
         auto prefix_begin = std::find_if(random_prefix.begin(), random_prefix.end(),
             [](const std::string& str) { return !str.empty(); });
+        // Append the prefix
         words.insert(words.begin(), prefix_begin, prefix_end);
 
+        // Starting prefix, can exit
         if ( random_prefix.front().empty() )
             break;
     }
+    // Determine the prefix obtained from the end of words
     Prefix last_prefix(prefix_size);
     std::size_t copy_size = math::min(prefix_size, words.size());
     for ( std::size_t i = 0; i < copy_size; i++ )
